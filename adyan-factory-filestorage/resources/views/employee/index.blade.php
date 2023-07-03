dfb8a69b6eb384743472ef2aaf4b39770861086f
spyreddd <adyanizza5353@gmail.com>
2023-07-03 23:19:09 +0700

    Factory-FileStorage

---
+++ adyan-factory-filestorage/resources/views/employee/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app-home')
+
+@section('content')
+    <div class="container mt-4">
+        <div class="row mb-0">
+            <div class="col-lg-9 col-xl-10">
+                <h4 class="mb-3">{{ $pageTitle }}</h4>
+            </div>
+            <div class="col-lg-3 col-xl-2">
+                <div class="d-grid gap-2">
+                    <a href="{{ route('employees.create') }}" class="btn btn-primary">Create Employee</a>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="table-responsive border p-3 rounded-3">
+            <table class="table table-bordered table-hover table-striped mb-0 bg-white">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($employees as $employee)
+                        <tr>
+                            <td>{{ $employee->firstname }}</td>
+                            <td>{{ $employee->lastname }}</td>
+                            <td>{{ $employee->email }}</td>
+                            <td>{{ $employee->age }}</td>
+                            <td>{{ $employee->position->name }}</td>
+                            <td>
+                                {{-- ACTIONS SECTION --}}
+                                <div class="d-flex">
+                                    <a href="{{ route('employees.show', ['employee' => $employee->id]) }}"
+                                        class="btn btn-primary btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                    <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}"
+                                        class="btn btn-warning btn-sm me-2"><i class="bi-pencil-square"></i></a>
+
+                                    <div>
+                                        <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit" class="btn btn-danger btn-sm me-2"><i
+                                                    class="bi-trash"></i></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
---
+++ adyan-factory-filestorage/resources/views/layouts/nav.blade.php
@@ -0,0 +1,68 @@
+@php
+    $currentRouteName = Route::currentRouteName();
+@endphp
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <div class="container">
+            <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Data Master</a>
+
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <hr class="d-md-none text-white-50">
+
+                <ul class="navbar-nav flex-row flex-wrap">
+                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('home') }}" class="nav-link @if($currentRouteName == 'home') active @endif">Home</a></li>
+                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('employees.index') }}" class="nav-link @if($currentRouteName == 'employees.index') active @endif">Employee</a></li>
+
+                </ul>
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 my-2 ms-md-auto">
+                <ul class="navbar-nav flex-row flex-wrap">
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+                <hr class="d-md-none text-white-50">
+
+                {{-- <a href="" class="btn btn-outline-light my-2 ms-md-auto"><i class="bi-person-circle me-1"></i> My Profile</a> --}}
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
